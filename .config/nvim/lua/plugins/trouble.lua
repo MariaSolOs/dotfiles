ca13b6b04da3339d74807334bd113d409d9bbaf6
Maria José Solano <majosolano99@gmail.com>
2023-06-10 08:52:10 -0500

    Remove auto trouble options

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -3,11 +3,6 @@ return {
     'folke/trouble.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     cmd = 'Trouble',
-    opts = {
-        -- Open the list when there's a diagnostic, and close it when there's none.
-        auto_open = true,
-        auto_close = true
-    },
     init = function(_)
         vim.keymap.set('n', '<leader>x', '<cmd>TroubleToggle<cr>',
             { silent = true, noremap = true }
