4e6fe98ab7556ef498a60e0fa843c9934c919329
Maria José Solano <majosolano99@gmail.com>
2023-06-12 19:40:10 -0700

    Remove unused mappings

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -19,8 +19,6 @@ return {
             end, { desc = '[/] Fuzzily search in current buffer' })
             vim.keymap.set('n', '<leader>sf', telescope_builtin.find_files, { desc = '[S]earch [F]iles' })
             vim.keymap.set('n', '<leader>sh', telescope_builtin.help_tags, { desc = '[S]earch [H]elp' })
-            vim.keymap.set('n', '<leader>sw', telescope_builtin.grep_string,
-                { desc = '[S]earch current [W]ord' })
             vim.keymap.set('n', '<leader>sg', telescope_builtin.live_grep, { desc = '[S]earch by [G]rep' })
             vim.keymap.set('n', '<leader>sd', telescope_builtin.diagnostics,
                 { desc = '[S]earch [D]iagnostics' })
