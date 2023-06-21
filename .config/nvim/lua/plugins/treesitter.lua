03b60d1653519bc563c549121fd62732bd9790d2
Maria José Solano <majosolano99@gmail.com>
2023-06-20 20:23:12 -0700

    Intuitive TS mapping

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -29,28 +29,20 @@ return {
                 },
             },
             textobjects = {
-                select = {
-                    enable = true,
-                    lookahead = true,
-                    keymaps = {
-                        ['af'] = '@function.outer',
-                        ['if'] = '@function.inner',
-                    },
-                },
                 move = {
                     enable = true,
                     set_jumps = true,
                     goto_next_start = {
-                        [']m'] = '@function.outer',
+                        [']f'] = '@function.outer',
                     },
                     goto_next_end = {
-                        [']M'] = '@function.outer',
+                        [']F'] = '@function.outer',
                     },
                     goto_previous_start = {
-                        ['[m'] = '@function.outer',
+                        ['[f'] = '@function.outer',
                     },
                     goto_previous_end = {
-                        ['[M'] = '@function.outer',
+                        ['[F'] = '@function.outer',
                     },
                 },
             },
