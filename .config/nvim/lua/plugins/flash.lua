d939d9bbeab422bb53dda45bb1f743179a4a7952
Maria José Solano <majosolano99@gmail.com>
2023-06-21 11:24:43 -0700

    Replace leap by flash

---
+++ .config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,28 @@
+-- Navigation with jump motions.
+return {
+    {
+        'folke/flash.nvim',
+        event = 'VeryLazy',
+        opts = {
+            modes = {
+                char = {
+                    enabled = false,
+                },
+            },
+        },
+        keys = {
+            {
+                's',
+                mode = { 'n', 'x', 'o' },
+                function()
+                    require('flash').jump()
+                end,
+                desc = 'Flash Jump',
+            },
+        },
+        init = function()
+            vim.api.nvim_set_hl(0, 'FlashBackdrop', { italic = true })
+            vim.api.nvim_set_hl(0, 'FlashCurrent', { fg = '#000000', bg = '#DD58DC' })
+        end,
+    },
+}
