88f2ca86ee59932dd0fd04e562bb9b757b05652e
Maria José Solano <majosolano99@gmail.com>
2023-06-23 11:08:00 -0700

    Add extra minifiles mappings

--- .config/nvim/lua/plugins/mini-files.lua
+++ .config/nvim/lua/plugins/mini-files.lua
@@ -1,4 +1,38 @@
--- File explorer.
+local nmap = require('helpers.keybindings').nmap
+
+-- Utilities for hiding/showing dotfiles.
+local show_dotfiles = true
+local filter_show = function()
+    return true
+end
+local filter_hide = function(file)
+    return not vim.startswith(file.name, '.')
+end
+local toggle_dotfiles = function()
+    show_dotfiles = not show_dotfiles
+    local new_filter = show_dotfiles and filter_show or filter_hide
+    require('mini.files').refresh { content = { filter = new_filter } }
+end
+
+-- Utilities for creating splits.
+local map_split = function(buf_id, lhs, direction)
+    local rhs = function()
+        local minifiles = require 'mini.files'
+
+        -- Make a new window and set it as target.
+        local new_target_window
+        vim.api.nvim_win_call(minifiles.get_target_window(), function()
+            vim.cmd(direction .. ' split')
+            new_target_window = vim.api.nvim_get_current_win()
+        end)
+
+        minifiles.set_target_window(new_target_window)
+    end
+
+    local desc = 'Split ' .. string.sub(direction, 12)
+    nmap(lhs, rhs, { buffer = buf_id, desc = desc })
+end
+
 return {
     {
         'echasnovski/mini.files',
@@ -29,6 +63,25 @@ return {
                 callback = function(args)
                     local win_id = args.data.win_id
                     vim.api.nvim_win_set_config(win_id, { border = 'rounded' })
+                end,
+            })
+
+            -- Show/hide dotfiles.
+            vim.api.nvim_create_autocmd('User', {
+                pattern = 'MiniFilesBufferCreate',
+                callback = function(args)
+                    local buf_id = args.data.buf_id
+                    vim.keymap.set('n', 't.', toggle_dotfiles, { buffer = buf_id, desc = 'Toggle dotfiles' })
+                end,
+            })
+
+            -- Open files in splits.
+            vim.api.nvim_create_autocmd('User', {
+                pattern = 'MiniFilesBufferCreate',
+                callback = function(args)
+                    local buf_id = args.data.buf_id
+                    map_split(buf_id, '<C-s>', 'belowright horizontal')
+                    map_split(buf_id, '<C-v>', 'belowright vertical')
                 end,
             })
         end,
