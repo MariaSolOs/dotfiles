220b8241b24b89f05c764e8203a7b1c5bc11571e
Maria José Solano <majosolano99@gmail.com>
2023-06-25 00:03:34 -0700

    Remove extra snippets

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -38,7 +38,6 @@ return {
             'L3MON4D3/LuaSnip',
             'saadparwaiz1/cmp_luasnip',
             'hrsh7th/cmp-nvim-lsp',
-            'rafamadriz/friendly-snippets',
             'hrsh7th/cmp-buffer',
         },
         version = false,
