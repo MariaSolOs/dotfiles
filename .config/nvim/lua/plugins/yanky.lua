496067498bcfbf62c4d85da1b6f264cc6ae84c7e
Maria José Solano <majosolano99@gmail.com>
2023-06-28 15:11:56 -0700

    Add yanky

---
+++ .config/nvim/lua/plugins/yanky.lua
@@ -0,0 +1,25 @@
+-- Nicer pasting.
+return {
+    'gbprod/yanky.nvim',
+    opts = {
+        ring = {
+            history_length = 20,
+        },
+        highlight = {
+            timer = 250,
+        },
+    },
+    keys = {
+        {
+            '<leader>sy',
+            function()
+                require('telescope').extensions.yank_history.yank_history {}
+            end,
+            desc = 'Search yank history',
+        },
+        { 'p', '<Plug>(YankyPutAfter)', mode = { 'n', 'x' }, desc = 'Put yanked text after cursor' },
+        { 'P', '<Plug>(YankyPutBefore)', mode = { 'n', 'x' }, desc = 'Put yanked text before cursor' },
+        { '=p', '<Plug>(YankyPutAfterFilter)', desc = 'Put yanked text in line below' },
+        { '=P', '<Plug>(YankyPutBeforeFilter)', desc = 'Put yanked text in line above' },
+    },
+}
