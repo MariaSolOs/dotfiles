23aea97d4d602712b4b0c689975234562c4cff9b
Maria José Solano <majosolano99@gmail.com>
2023-06-30 23:29:39 -0700

    Remove copilot autotrigger

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -4,14 +4,12 @@ return {
         'zbirenbaum/copilot.lua',
         cmd = 'Copilot',
         event = 'InsertEnter',
-        dependencies = 'nvim-cmp',
         opts = {
             -- I don't find the panel useful.
             panel = {
                 enabled = false,
             },
             suggestion = {
-                auto_trigger = true,
                 -- Use alt to interact with Copilot.
                 keymap = {
                     -- Disable the built-in mapping, we'll configure it in nvim-cmp.
@@ -29,38 +27,15 @@ return {
         },
         config = function(_, opts)
             local cmp = require 'cmp'
-            local copilot = require 'copilot.suggestion'
-            local luasnip = require 'luasnip'
-
             require('copilot').setup(opts)
-
-            local dismiss = function()
-                if copilot.is_visible() then
-                    copilot.dismiss()
-                end
-            end
-
-            local set_trigger = function(trigger)
-                vim.b.copilot_suggestion_auto_trigger = trigger
-                vim.b.copilot_suggestion_hidden = not trigger
-            end
 
             -- Hide suggestions when the completion menu is open.
             cmp.event:on('menu_opened', function()
-                dismiss()
-                set_trigger(false)
+                vim.b.copilot_suggestion_hidden = true
             end)
             cmp.event:on('menu_closed', function()
-                set_trigger(not luasnip.expand_or_jumpable())
+                vim.b.copilot_suggestion_hidden = false
             end)
-
-            -- Disable suggestions when inside a snippet.
-            vim.api.nvim_create_autocmd('User', {
-                pattern = { 'LuasnipInsertNodeEnter', 'LuasnipInsertNodeLeave' },
-                callback = function()
-                    set_trigger(not luasnip.expand_or_locally_jumpable())
-                end,
-            })
         end,
     },
 }
