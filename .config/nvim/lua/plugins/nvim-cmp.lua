15bb8b32ff270b1df61b37f1fd814839108f5bd1
Maria José Solano <majosolano99@gmail.com>
2023-07-06 09:19:54 -0700

    luasnip exit snippet hack

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -36,11 +36,29 @@ return {
             {
                 'L3MON4D3/LuaSnip',
                 config = function()
+                    local luasnip = require 'luasnip'
                     local types = require 'luasnip.util.types'
 
                     require('luasnip.loaders.from_vscode').lazy_load()
 
-                    require('luasnip').setup {
+                    -- HACK: Cancel the snippet session when leaving insert mode.
+                    vim.api.nvim_create_autocmd('ModeChanged', {
+                        pattern = '*',
+                        callback = function()
+                            if
+                                (
+                                    (vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n')
+                                    or vim.v.event.old_mode == 'i'
+                                )
+                                and luasnip.session.current_nodes[vim.api.nvim_get_current_buf()]
+                                and not luasnip.session.jump_active
+                            then
+                                luasnip.unlink_current()
+                            end
+                        end,
+                    })
+
+                    luasnip.setup {
                         -- Display a cursor-like placeholder in unvisited nodes
                         -- of the snippet.
                         ext_opts = {
