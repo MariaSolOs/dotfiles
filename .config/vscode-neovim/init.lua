64a8eaee3705230f0152a67d4b0d102a4e683293
Maria José Solano <majosolano99@gmail.com>
2023-07-05 22:56:57 -0700

    more vscode configs

--- .config/vscode-neovim/init.lua
+++ .config/vscode-neovim/init.lua
@@ -12,19 +12,31 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- [[ Keymaps ]]
--- Make the leader a noop when not followed by something.
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+-- [[ Settings ]]
+-- Sync clipboard between the OS and Neovim.
+vim.o.clipboard = 'unnamedplus'
 
--- Window navigation.
+-- Case insensitive searching UNLESS /C or the search has capitals.
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Disable some of those annoying hit-enter messages.
+vim.opt.shortmess:append 'IWs'
+
+-- [[ Keymaps ]]
+-- Window/tab navigation.
 vim.keymap.set('n', '<C-h>', ":call VSCodeNotify('workbench.action.focusLeftGroup')<cr>")
 vim.keymap.set('n', '<C-j>', ":call VSCodeNotify('workbench.action.focusBelowGroup')<cr>")
 vim.keymap.set('n', '<C-k>', ":call VSCodeNotify('workbench.action.focusAboveGroup')<cr>")
 vim.keymap.set('n', '<C-l>', ":call VSCodeNotify('workbench.action.focusRightGroup')<cr>")
+vim.keymap.set('n', '[t', ":call VSCodeNotify('workbench.action.previousEditorInGroup')<cr>")
+vim.keymap.set('n', ']t', ":call VSCodeNotify('workbench.action.nextEditorInGroup')<cr>")
 
--- Sync clipboard between the OS and Neovim.
-vim.o.clipboard = 'unnamedplus
+-- Extra language service mappings.
+vim.keymap.set('n', 'gt', ":call VSCodeNotify('editor.action.goToTypeDefinition')<cr>")
+vim.keymap.set('n', 'gr', ":call VSCodeNotify('references-view.findReferences')<cr>")
 
+-- [[ Autocommands ]]
 -- Highlight on yank.
 vim.api.nvim_create_autocmd('TextYankPost', {
     group = vim.api.nvim_create_augroup('YankHighlight', {}),
@@ -35,38 +47,51 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- Configure plugins.
-require('lazy').setup({
+require('lazy').setup (
     {
-        'nvim-treesitter/nvim-treesitter',
-        event = { 'BufReadPost', 'BufNewFile' },
-        opts = {
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = '<cr>',
-                    node_incremental = '<cr>',
-                    scope_incremental = false,
-                    node_decremental = '<bs>',
+        {
+            'folke/flash.nvim',
+            event = 'VeryLazy',
+            config = true,
+            keys = {
+                {
+                    's',
+                    mode = { 'n', 'x', 'o' },
+                    function()
+                        require('flash').jump()
+                    end,
                 },
             },
         },
-        config = function(_, opts)
-            require('nvim-treesitter.configs').setup(opts)
-        end,
-    },
-
-    {
-        'folke/flash.nvim',
-        event = 'VeryLazy',
-        keys = {
-            {
-                's',
-                mode = { 'n', 'x', 'o' },
-                function()
-                    require('flash').jump()
-                end,
+        {
+            'nvim-treesitter/nvim-treesitter',
+            dependencies = 'nvim-treesitter/nvim-treesitter-textobjects',
+            event = { 'BufReadPost', 'BufNewFile' },
+            opts = {
+                highlight = {
+                    enable = false
+                },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = '<cr>',
+                        node_incremental = '<cr>',
+                        scope_incremental = false,
+                        node_decremental = '<bs>',
+                    },
+                },
             },
+            config = function(_, opts)
+                require('nvim-treesitter.configs').setup(opts)
+            end
         },
-        config = true
-    },
-})
+    }, 
+    {
+        checker = {
+            enabled = false
+        },
+        change_detection = {
+            enabled = false
+        }
+    }
+)
