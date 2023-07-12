990d6741cfbfec8fc40d148c29fbab222b535b8e
Maria José Solano <majosolano99@gmail.com>
2023-07-11 19:04:53 -0700

    Remove yank history mapping

--- .config/nvim/lua/plugins/yanky.lua
+++ .config/nvim/lua/plugins/yanky.lua
@@ -10,13 +10,6 @@ return {
         },
     },
     keys = {
-        {
-            '<leader>ty',
-            function()
-                require('telescope').extensions.yank_history.yank_history {}
-            end,
-            desc = 'Yank history',
-        },
         { '=y', '<Plug>(YankyYank)', mode = { 'n', 'x' }, desc = 'Yanky yank' },
         { 'p', '<Plug>(YankyPutAfter)', mode = { 'n', 'x' }, desc = 'Put yanked text after cursor' },
         { 'P', '<Plug>(YankyPutBefore)', mode = { 'n', 'x' }, desc = 'Put yanked text before cursor' },
