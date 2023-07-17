1ab0f259fc299f5e7f09ecc54e484206c5d87a5b
Maria José Solano <majosolano99@gmail.com>
2023-07-16 19:29:04 -0700

    re-install crates.nvim

---
+++ .config/nvim/lua/plugins/crates.lua
@@ -0,0 +1,27 @@
+-- Managing crate dependencies.
+return {
+    {
+        'Saecki/crates.nvim',
+        event = 'BufRead Cargo.toml',
+        opts = {
+            popup = {
+                border = 'rounded',
+            },
+        },
+        config = function(_, opts)
+            local crates = require 'crates'
+
+            crates.setup(opts)
+
+            local keymap = function(key, rhs, desc)
+                vim.keymap.set('n', '<leader>c' .. key, function()
+                    rhs()
+                    crates.focus_popup()
+                end, { desc = desc, silent = true })
+            end
+            keymap('c', crates.show_crate_popup, 'Show crate popup')
+            keymap('f', crates.show_features_popup, 'Show features popup')
+            keymap('v', crates.show_versions_popup, 'Show versions popup')
+        end,
+    },
+}
--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -147,6 +147,7 @@ return {
                     { name = 'nvim_lsp' },
                     { name = 'luasnip' },
                     { name = 'buffer' },
+                    { name = 'crates' },
                 },
             }
         end,
