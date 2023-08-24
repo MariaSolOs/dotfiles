f7402e6bb53040b62bc719b5a21d3339a25ba399
Maria José Solano <majosolano99@gmail.com>
2023-08-23 17:00:34 -0700

    add git-toggling file picker

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -1,5 +1,30 @@
 local symbol_icons = require('icons').symbol_kinds
 
+-- Custom files picker with toggling for respecting/ignoring .gitignore.
+local FilesPicker = { opts = nil }
+FilesPicker.toggle = function(_, _)
+    FilesPicker.pick(FilesPicker.opts)
+end
+FilesPicker.pick = function(opts)
+    opts = opts or {}
+    opts.cmd = opts.cmd or 'fd --color=never --type f --hidden --follow --no-ignore'
+    opts.actions = {
+        ['ctrl-g'] = FilesPicker.toggle,
+    }
+    local behavior = ''
+    if opts.cmd:match '%s+%-%-no%-ignore$' then
+        behavior = 'respecting'
+        opts.cmd = opts.cmd:gsub('%s+%-%-no%-ignore$', '')
+    else
+        behavior = 'ignoring'
+        opts.cmd = opts.cmd .. ' --no-ignore'
+    end
+    opts.winopts = { title = 'Files (' .. behavior .. ' .gitignore)' }
+    FilesPicker.opts = opts
+
+    require('fzf-lua').files(opts)
+end
+
 -- Picker, finder, etc.
 return {
     {
@@ -28,51 +53,58 @@ return {
             },
             { '<leader>fc', '<cmd>FzfLua highlights<cr>', desc = 'Highlights' },
             { '<leader>fd', '<cmd>FzfLua lsp_workspace_diagnostics<cr>', desc = 'Diagnostics' },
-            { '<leader>ff', '<cmd>FzfLua files<cr>', desc = 'Find files' },
+            { '<leader>ff', FilesPicker.pick, desc = 'Find files' },
             { '<leader>fg', '<cmd>FzfLua live_grep<cr>', desc = 'Grep' },
             { '<leader>fh', '<cmd>FzfLua help_tags<cr>', desc = 'Help' },
             { '<leader>fr', '<cmd>FzfLua oldfiles<cr>', desc = 'Recently opened files' },
         },
-        opts = {
-            -- Make stuff better combine with the editor.
-            fzf_colors = {
-                bg = { 'bg', 'Normal' },
-                gutter = { 'bg', 'Normal' },
-                info = { 'fg', 'Conditional' },
-                scrollbar = { 'bg', 'Normal' },
-                separator = { 'fg', 'Comment' },
-            },
-            fzf_opts = {
-                ['--info'] = 'default',
-            },
-            keymap = {
-                builtin = {
-                    ['<C-/>'] = 'toggle-help',
-                    ['<C-a>'] = 'toggle-fullscreen',
-                    ['<C-i>'] = 'toggle-preview',
-                    ['<C-f>'] = 'preview-page-down',
-                    ['<C-b>'] = 'preview-page-up',
+        config = function()
+            local fzf = require 'fzf-lua'
+
+            -- Add the .gitignore toggle description for the files picker.
+            require('fzf-lua.config').set_action_helpstr(FilesPicker.toggle, 'no-ignore<->ignore')
+
+            fzf.setup {
+                -- Make stuff better combine with the editor.
+                fzf_colors = {
+                    bg = { 'bg', 'Normal' },
+                    gutter = { 'bg', 'Normal' },
+                    info = { 'fg', 'Conditional' },
+                    scrollbar = { 'bg', 'Normal' },
+                    separator = { 'fg', 'Comment' },
                 },
-                fzf = {
-                    ['alt-s'] = 'toggle',
-                    ['alt-a'] = 'toggle-all',
+                fzf_opts = {
+                    ['--info'] = 'default',
                 },
-            },
-            winopts = {
-                preview = { scrollbar = false },
-            },
-            -- Configuration for specific commands.
-            files = { git_icons = false },
-            oldfiles = { include_current_session = true },
-            grep = {
-                git_icons = false,
-                header_prefix = ' ',
-            },
-            lsp = {
-                symbols = {
-                    symbol_icons = symbol_icons,
+                keymap = {
+                    builtin = {
+                        ['<C-/>'] = 'toggle-help',
+                        ['<C-a>'] = 'toggle-fullscreen',
+                        ['<C-i>'] = 'toggle-preview',
+                        ['<C-f>'] = 'preview-page-down',
+                        ['<C-b>'] = 'preview-page-up',
+                    },
+                    fzf = {
+                        ['alt-s'] = 'toggle',
+                        ['alt-a'] = 'toggle-all',
+                    },
                 },
-            },
-        },
+                winopts = {
+                    preview = { scrollbar = false },
+                },
+                -- Configuration for specific commands.
+                files = { git_icons = false },
+                oldfiles = { include_current_session = true },
+                grep = {
+                    git_icons = false,
+                    header_prefix = ' ',
+                },
+                lsp = {
+                    symbols = {
+                        symbol_icons = symbol_icons,
+                    },
+                },
+            }
+        end,
     },
 }
