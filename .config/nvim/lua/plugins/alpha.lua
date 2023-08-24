a4416526da75acbc5649dc34d3895049a6e242a2
Maria José Solano <majosolano99@gmail.com>
2023-08-24 11:32:21 -0700

    move devicons to alpha

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -2,6 +2,7 @@
 return {
     {
         'goolord/alpha-nvim',
+        dependencies = 'nvim-tree/nvim-web-devicons',
         event = 'VimEnter',
         config = function()
             local dashboard = require 'alpha.themes.dashboard'
--- .config/nvim/lua/plugins/nvim-web-devicons.lua
+++
@@ -1,4 +0,0 @@
--- Used by a bunch of plugins.
-return {
-    { 'nvim-tree/nvim-web-devicons', lazy = true },
-}
