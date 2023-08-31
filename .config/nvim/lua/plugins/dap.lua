22249f64385427a5ce4d58213f7b67f54027b68a
Maria José Solano <majosolano99@gmail.com>
2023-08-30 20:15:02 -0700

    force mv when installing vscode jsdebug

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -87,7 +87,7 @@ return {
             {
                 'microsoft/vscode-js-debug',
                 version = '1.x',
-                build = 'npm i && npm run compile vsDebugServerBundle && mv dist out',
+                build = 'npm i && npm run compile vsDebugServerBundle && mv -f dist out',
             },
             -- Lua adapter.
             {
