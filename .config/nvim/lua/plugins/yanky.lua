6c707baecc26098beee82a639903ca6ad99e2436
Maria José Solano <majosolano99@gmail.com>
2023-09-16 18:54:57 -0700

    Yanky tweaks

--- .config/nvim/lua/plugins/yanky.lua
+++ .config/nvim/lua/plugins/yanky.lua
@@ -6,10 +6,11 @@ return {
         highlight = { timer = 250 },
     },
     keys = {
+        { 'y', '<Plug>(YankyYank)', mode = { 'n', 'x' }, desc = 'Yank' },
         { 'p', '<Plug>(YankyPutAfter)', mode = { 'n', 'x' }, desc = 'Put yanked text after cursor' },
         { 'P', '<Plug>(YankyPutBefore)', mode = { 'n', 'x' }, desc = 'Put yanked text before cursor' },
-        { '=p', '<Plug>(YankyPutAfterFilter)', desc = 'Put yanked text in line below' },
-        { '=P', '<Plug>(YankyPutBeforeFilter)', desc = 'Put yanked text in line above' },
+        { '=p', '<Plug>(YankyPutAfterLinewise)', desc = 'Put yanked text in line below' },
+        { '=P', '<Plug>(YankyPutBeforeLinewise)', desc = 'Put yanked text in line above' },
         { '[y', '<Plug>(YankyCycleForward)', desc = 'Cycle forward through yank history' },
         { ']y', '<Plug>(YankyCycleBackward)', desc = 'Cycle backward through yank history' },
     },
