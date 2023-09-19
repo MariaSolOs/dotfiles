39874568df70a47e9e7d3939ecffb88fccc621eb
Maria José Solano <majosolano99@gmail.com>
2023-09-18 18:23:35 -0700

    Add forced buffer delete

--- .config/nvim/lua/plugins/minibufremove.lua
+++ .config/nvim/lua/plugins/minibufremove.lua
@@ -11,6 +11,13 @@ return {
                 end,
                 desc = 'Delete current buffer',
             },
+            {
+                '<leader>bD',
+                function()
+                    require('mini.bufremove').delete(0, true)
+                end,
+                desc = 'Delete (!) current buffer',
+            },
         },
     },
 }
