589eabb83f4087efb95d0c2a994371ebeca19e09
Maria José Solano <majosolano99@gmail.com>
2023-10-01 12:41:01 -0700

    Scratch buffer command

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -2,6 +2,20 @@ vim.api.nvim_create_user_command('Todos', function()
     require('fzf-lua').grep { search = [[TODO:|todo!\(.*\)]], no_esc = true }
 end, { desc = 'Grep TODOs', nargs = 0 })
 
+vim.api.nvim_create_user_command('Scratch', function()
+    vim.cmd 'bel 10new'
+    local buf = vim.api.nvim_get_current_buf()
+    for name, value in pairs {
+        filetype = 'scratch',
+        buftype = 'nofile',
+        bufhidden = 'hide',
+        swapfile = false,
+        modifiable = true,
+    } do
+        vim.api.nvim_set_option_value(name, value, { buf = buf })
+    end
+end, { desc = 'Open a scratch buffer', nargs = 0 })
+
 vim.api.nvim_create_autocmd('FileType', {
     group = vim.api.nvim_create_augroup('mariasolos/close_with_q', { clear = true }),
     desc = 'Close with <q>',
@@ -10,6 +24,7 @@ vim.api.nvim_create_autocmd('FileType', {
         'man',
         'qf',
         'query',
+        'scratch',
         'spectre_panel',
         'kitty_scrollback',
     },
