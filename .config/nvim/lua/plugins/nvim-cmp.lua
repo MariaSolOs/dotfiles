a5e38fe69e6759aa7bfb42e64090dfac92ff79a7
Maria José Solano <majosolano99@gmail.com>
2023-10-04 23:27:59 -0700

    Trying out custom cmp comparators

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -65,9 +65,10 @@ return {
             'saadparwaiz1/cmp_luasnip',
         },
         version = false,
-        event = { 'CmdlineEnter', 'InsertEnter' },
+        event = 'InsertEnter',
         opts = function()
             local cmp = require 'cmp'
+            local compare = require 'cmp.config.compare'
             local luasnip = require 'luasnip'
             local symbol_kinds = require('icons').symbol_kinds
 
@@ -157,6 +158,16 @@ return {
                 }, {
                     { name = 'buffer', keyword_length = 4 },
                 }),
+                sorting = {
+                    comparators = {
+                        compare.offset,
+                        compare.exact,
+                        compare.score,
+                        compare.recently_used,
+                        compare.sort_text,
+                        compare.length,
+                    },
+                },
             }
         end,
         config = function(_, opts)
