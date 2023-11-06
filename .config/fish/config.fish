d5235ab03fdc514f1cda59fcafbe622c2b33c928
Maria José Solano <majosolano99@gmail.com>
2023-11-05 20:00:53 -0800

    Fix vim cursor

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -28,6 +28,7 @@ set -U fish_greeting
 
 # Vi mode.
 set -g fish_key_bindings fish_vi_key_bindings
+set fish_vi_force_cursor 1
 set fish_cursor_default block
 set fish_cursor_insert line
 set fish_cursor_replace_one underscore
