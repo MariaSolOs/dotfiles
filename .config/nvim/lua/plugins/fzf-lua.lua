1c7bef34f4efe4b96c02d06c6b26a3fb11d26a99
Maria José Solano <majosolano99@gmail.com>
2023-11-06 10:19:34 -0800

    fzf files picker now has gitignore toggle

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -1,40 +1,5 @@
 local icons = require 'icons'
 
--- Custom files picker with toggling for respecting/ignoring .gitignore.
-local FilesPicker = {
-    opts = nil,
-    ignoring = nil,
-}
-FilesPicker.toggle = function()
-    FilesPicker.pick(FilesPicker.opts)
-end
-FilesPicker.pick = function(opts)
-    -- Always start by respecting .gitignore (will be toggled below).
-    if not opts then
-        FilesPicker.ignoring = true
-    end
-
-    -- Settings to always apply.
-    opts = opts or { actions = { ['ctrl-g'] = FilesPicker.toggle } }
-    opts.winopts = opts.winopts or { title_pos = 'center' }
-    opts.cmd = 'fd --color=never --type f --hidden --follow'
-
-    local behavior = ''
-    if FilesPicker.ignoring then
-        behavior = 'respecting'
-        opts.cmd = opts.cmd .. ' --exclude .git'
-    else
-        behavior = 'ignoring'
-        opts.cmd = opts.cmd .. ' --no-ignore'
-    end
-    opts.winopts.title = string.format('Files (%s .gitignore)', behavior)
-
-    FilesPicker.ignoring = not FilesPicker.ignoring
-    FilesPicker.opts = opts
-
-    require('fzf-lua').files(opts)
-end
-
 -- Picker, finder, etc.
 return {
     {
@@ -58,7 +23,7 @@ return {
             { '<leader>fc', '<cmd>FzfLua highlights<cr>', desc = 'Highlights' },
             { '<leader>fd', '<cmd>FzfLua lsp_document_diagnostics<cr>', desc = 'Document diagnostics' },
             { '<leader>fD', '<cmd>FzfLua lsp_workspace_diagnostics<cr>', desc = 'Workspace diagnostics' },
-            { '<leader>ff', FilesPicker.pick, desc = 'Find files' },
+            { '<leader>ff', '<cmd>FzfLua files<cr>', desc = 'Find files' },
             { '<leader>fg', '<cmd>FzfLua live_grep_glob<cr>', desc = 'Grep' },
             { '<leader>fg', '<cmd>FzfLua grep_visual<cr>', desc = 'Grep', mode = 'x' },
             { '<leader>fh', '<cmd>FzfLua help_tags<cr>', desc = 'Help' },
@@ -108,6 +73,9 @@ return {
                     winopts = {
                         preview = { hidden = 'hidden' },
                     },
+                    actions = {
+                        ['ctrl-g'] = actions.toggle_ignore,
+                    },
                 },
                 grep = {
                     header_prefix = icons.misc.search .. ' ',
@@ -130,12 +98,6 @@ return {
                     },
                 },
             }
-        end,
-        config = function(_, opts)
-            require('fzf-lua').setup(opts)
-
-            -- Add the .gitignore toggle description for the files picker.
-            require('fzf-lua.config').set_action_helpstr(FilesPicker.toggle, 'no-ignore<->ignore')
         end,
     },
 }
