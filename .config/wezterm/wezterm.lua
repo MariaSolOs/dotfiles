a8e398ad622b2b8d1310c2d08625dd30c52b4887
Maria José Solano <majosolano99@gmail.com>
2023-11-12 18:58:26 -0800

    Custom wezterm tab titles

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require 'wezterm'
+local icons = wezterm.nerdfonts
 local act = wezterm.action
 local config = wezterm.config_builder()
 
@@ -112,5 +113,29 @@ config.keys = {
     { mods = 'SUPER', key = 'c', action = act.CopyTo 'Clipboard' },
     { mods = 'SUPER', key = 'v', action = act.PasteFrom 'Clipboard' },
 }
+
+local process_icons = {
+    fish = icons.md_fish,
+    lazygit = icons.dev_git_merge,
+    -- TODO: Replace this icon by the neovim one when available in Nerd Fonts.
+    nvim = icons.fa_code,
+    zsh = icons.dev_terminal_badge,
+}
+wezterm.on('format-tab-title', function(tab, _, _, _, _, max_width)
+    -- Use the icon for the process, falling back to the process name.
+    local process = string.gsub(tab.active_pane.foreground_process_name, '(.*[/\\])(.*)', '%2')
+    process = process_icons[process] or process
+
+    -- When there's no process info, just show a laptop icon.
+    process = #process > 0 and process or icons.md_laptop
+
+    -- Current working directory.
+    local cwd = tab.active_pane.current_working_dir
+    cwd = cwd and string.format('%s ', cwd.file_path:gsub(os.getenv 'HOME', '~')) or ''
+
+    -- Return the title, making sure it fits.
+    local title = string.format('(%d %s) %s', tab.tab_index + 1, process, cwd)
+    return wezterm.truncate_right(title, max_width)
+end)
 
 return config
