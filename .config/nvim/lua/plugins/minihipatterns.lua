5567798e4b193b2bddaacb4a04d537beb53d77b0
Maria José Solano <majosolano99@gmail.com>
2023-12-09 18:07:41 -0800

    feat(nvim): re-install ccc

--- .config/nvim/lua/plugins/minihipatterns.lua
+++ .config/nvim/lua/plugins/minihipatterns.lua
@@ -4,9 +4,7 @@ return {
         'echasnovski/mini.hipatterns',
         event = 'BufReadPost',
         opts = function()
-            local highlighters = {
-                hex_color = require('mini.hipatterns').gen_highlighter.hex_color(),
-            }
+            local highlighters = {}
             for _, word in ipairs { 'todo', 'note', 'hack' } do
                 highlighters[word] = {
                     pattern = string.format('%%f[%%w]()%s()%%f[%%W]', word:upper()),
