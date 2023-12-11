f817d66b0923c5c07de75e9e8207f2ae6c3bddfd
Maria José Solano <majosolano99@gmail.com>
2023-12-10 19:48:02 -0800

    fix(dunst): greyish backlight notifications

--- .config/hypr/scripts/backlight.sh
+++ .config/hypr/scripts/backlight.sh
@@ -2,24 +2,16 @@
 
 notify_backlight() {
 	# Divide the current brightness by the maximum and multiply by 100 to get a percentage.
-	local light=$(printf "%.0f\n" $((100 * $(brightnessctl g) / $(brightnessctl m))))
+	local light
+	light=$(printf "%.0f\n" $((100 * $(brightnessctl g) / $(brightnessctl m))))
+
 	dunstify -a "Backlight" -h string:x-dunst-stack-tag:backlight -h int:value:"$light" ""
 }
 
-# Increase screen brightness.
-inc_backlight() {
-	brightnessctl s +5%
-}
-
-# Decrease screen brightness.
-dec_backlight() {
-	brightnessctl s 5%-
-}
-
 if [[ "$1" == "--inc" ]]; then
-	inc_backlight
+	brightnessctl s +5%
 elif [[ "$1" == "--dec" ]]; then
-	dec_backlight
+	brightnessctl s 5%-
 else
 	echo "Usage: backlight [--inc|--dec]"
 	exit 1
