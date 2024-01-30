88a146e140fbe25b40bc79be2c8e079ca3d01884
Maria José Solano <majosolano99@gmail.com>
2024-01-29 16:13:39 -0800

    fix(nvim): inlay hint truncation

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -155,6 +155,7 @@ vim.diagnostic.config {
 
 -- Override the virtual text diagnostic handler so that the most severe diagnostic is shown first.
 local show_handler = vim.diagnostic.handlers.virtual_text.show
+assert(show_handler)
 local hide_handler = vim.diagnostic.handlers.virtual_text.hide
 vim.diagnostic.handlers.virtual_text = {
     show = function(ns, bufnr, diagnostics, opts)
@@ -270,6 +271,25 @@ vim.lsp.util.stylize_markdown = function(bufnr, contents, opts)
     add_inline_highlights(bufnr)
 
     return contents
+end
+
+-- Workaround for truncating long TypeScript inlay hints.
+-- TODO: Remove this if https://github.com/neovim/neovim/issues/27240 gets addressed.
+local inlay_hint_handler = vim.lsp.handlers[methods.textDocument_inlayHint]
+vim.lsp.handlers[methods.textDocument_inlayHint] = function(err, result, ctx, config)
+    local client = vim.lsp.get_client_by_id(ctx.client_id)
+    if client and client.name == 'typescript-tools' then
+        result = vim.iter.map(function(hint)
+            local label = hint.label ---@type string
+            if label:len() >= 30 then
+                label = label:sub(1, 29) .. '…'
+            end
+            hint.label = label
+            return hint
+        end, result)
+    end
+
+    inlay_hint_handler(err, result, ctx, config)
 end
 
 -- Update mappings when registering dynamic capabilities.
