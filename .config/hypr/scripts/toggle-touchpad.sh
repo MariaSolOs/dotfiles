d9f2333e6328e6c0c2eb795f698b76130e2f2b51
Maria José Solano <majosolano99@gmail.com>
2024-02-24 17:46:20 -0800

    fix(hypr): overwrite toggletouchpad notification

--- .config/hypr/scripts/toggle-touchpad.sh
+++ .config/hypr/scripts/toggle-touchpad.sh
@@ -2,20 +2,37 @@
 
 function toggletouchpad() {
 	local setting='device:apple-internal-keyboard-/-trackpad-1:enabled'
+	local settingfile='/tmp/toggle-touchpad-notification-id'
+
+	if [ -f $settingfile ]; then
+		notificationid=$(cat $settingfile)
+	else
+		notificationid=0
+	fi
 
 	# Check if the touchpad is enabled.
 	local settingvalue
 	settingvalue=$(hyprctl getoption $setting | rg 'int: 1')
 
+	local icon message newsettingvalue
 	if [ -z "$settingvalue" ]; then
 		# The touchpad is disabled:
-		notify-send -i 'touchpad-enabled-symbolic' 'Enabling touchpad...'
-		hyprctl keyword $setting 1
+		icon='enabled'
+		message='Enabling'
+		newsettingvalue=1
 	else
 		# The touchpad is enabled:
-		notify-send -i 'touchpad-disabled-symbolic' 'Disabling touchpad...'
-		hyprctl keyword $setting 0
+		icon='disabled'
+		message='Disabling'
+		newsettingvalue=0
 	fi
+
+	# Notify and save the new ID.
+	notificationid=$(notify-send -p -r "$notificationid" -i "touchpad-$icon-symbolic" -a "hyprland" "$message touchpad...")
+	echo "$notificationid" >$settingfile
+
+	# Change the Hyprland setting.
+	hyprctl keyword $setting $newsettingvalue
 }
 
 toggletouchpad
