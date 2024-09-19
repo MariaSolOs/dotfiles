27b2ce0e3ba9ca4486aa3095d7fcb016297c1afd
Maria José Solano <majosolano99@gmail.com>
2024-09-19 13:11:48 -0700

    feat(nvim): toggle the toggling of inlay hints

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -79,31 +79,22 @@ local function on_attach(client, bufnr)
     end
 
     if client.supports_method(methods.textDocument_inlayHint) then
-        local inlay_hints_group = vim.api.nvim_create_augroup('mariasolos/toggle_inlay_hints', { clear = false })
+        vim.keymap.set('n', '<leader>ci', function()
+            -- Toggle the hints:
+            local enabled = vim.lsp.inlay_hint.is_enabled { bufnr = bufnr }
+            vim.lsp.inlay_hint.enable(not enabled, { bufnr = bufnr })
 
-        -- Initial inlay hint display.
-        -- Idk why but without the delay inlay hints aren't displayed at the very start.
-        vim.defer_fn(function()
-            local mode = vim.api.nvim_get_mode().mode
-            vim.lsp.inlay_hint.enable(mode == 'n' or mode == 'v', { bufnr = bufnr })
-        end, 500)
-
-        vim.api.nvim_create_autocmd('InsertEnter', {
-            group = inlay_hints_group,
-            desc = 'Enable inlay hints',
-            buffer = bufnr,
-            callback = function()
-                vim.lsp.inlay_hint.enable(false, { bufnr = bufnr })
-            end,
-        })
-        vim.api.nvim_create_autocmd('InsertLeave', {
-            group = inlay_hints_group,
-            desc = 'Disable inlay hints',
-            buffer = bufnr,
-            callback = function()
-                vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-            end,
-        })
+            -- If toggling them on, turn them back off when entering insert mode.
+            if not enabled then
+                vim.api.nvim_create_autocmd('InsertEnter', {
+                    buffer = bufnr,
+                    once = true,
+                    callback = function()
+                        vim.lsp.inlay_hint.enable(false, { bufnr = bufnr })
+                    end,
+                })
+            end
+        end, { buffer = bufnr })
     end
 end
 
