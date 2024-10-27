6dcf12b8a6fb23a8446b95a79c80908141688865
Maria José Solano <majosolano99@gmail.com>
2024-10-27 13:08:14 -0700

    fix(nvim): better indent fallback in folding

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -93,23 +93,18 @@ vim.api.nvim_create_autocmd('FileType', {
     callback = function(args)
         local bufnr = args.buf
 
-        -- Because of perf, just use indentation for folding in huge files.
-        if vim.bo[bufnr].filetype == 'bigfile' then
+        -- Enable Treesitter folding when not in huge files and when Treesitter
+        -- is working.
+        if vim.bo[bufnr].filetype ~= 'bigfile' and pcall(vim.treesitter.start, bufnr) then
+            vim.api.nvim_buf_call(bufnr, function()
+                vim.wo[0][0].foldmethod = 'expr'
+                vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+                vim.cmd.normal 'zx'
+            end)
+        else
+            -- Else just fallback to using indentation.
             vim.wo[0][0].foldmethod = 'indent'
-            return
         end
-
-        -- Check that Treesitter works.
-        if not pcall(vim.treesitter.start, bufnr) then
-            return
-        end
-
-        -- Enable Treesitter folding.
-        vim.api.nvim_buf_call(bufnr, function()
-            vim.wo[0][0].foldmethod = 'expr'
-            vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-            vim.cmd.normal 'zx'
-        end)
     end,
 })
 
