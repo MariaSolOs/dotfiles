139ae5fa4aac9511cea2904cbb86de5e5c73e618
Maria José Solano <majosolano99@gmail.com>
2024-10-27 15:21:20 -0700

    fix(fish): typos in completion paths

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -40,11 +40,11 @@ end
 
 # Add completions from stuff installed with Homebrew.
 if test "$os" = Darwin
-    if test -d (brew --prefix)"/share/fish/completions.d"
+    if test -d (brew --prefix)"/share/fish/completions"
         set -p fish_complete_path (brew --prefix)/share/fish/completions
     end
     if test -d (brew --prefix)"/share/fish/vendor_completions.d"
-        set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions
+        set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
     end
 end
 
