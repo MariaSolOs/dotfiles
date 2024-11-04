7ba390788b4c0e69735551eb1bc8a3ae4399896f
Maria José Solano <majosolano99@gmail.com>
2024-11-03 18:41:42 -0800

    feat(nvim): add notification to ToggleFormat command

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -1,5 +1,6 @@
 vim.api.nvim_create_user_command('ToggleFormat', function()
     vim.g.autoformat = not vim.g.autoformat
+    vim.notify(string.format('%s formatting...', vim.g.autoformat and 'Enabling' or 'Disabling'), vim.log.levels.INFO)
 end, { desc = 'Toggle conform.nvim auto-formatting', nargs = 0 })
 
 vim.api.nvim_create_user_command('Todos', function()
