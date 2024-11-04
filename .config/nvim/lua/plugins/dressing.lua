2c10f02f2f904181b6ec748f595ce1294ac16ecb
Maria José Solano <majosolano99@gmail.com>
2024-11-03 19:30:04 -0800

    fix(nvim): don't always load dressing

--- .config/nvim/lua/plugins/dressing.lua
+++ .config/nvim/lua/plugins/dressing.lua
@@ -52,15 +52,19 @@ return {
         init = function()
             ---@diagnostic disable-next-line: duplicate-set-field
             vim.ui.select = function(items, opts, on_choice)
+                if not package.loaded['dressing.nvim'] then
+                    require('lazy').load { plugins = { 'dressing.nvim' } }
+                end
                 -- Don't show the picker if there's nothing to pick.
                 if #items > 0 then
-                    require('lazy').load { plugins = { 'dressing.nvim' } }
                     return vim.ui.select(items, opts, on_choice)
                 end
             end
             ---@diagnostic disable-next-line: duplicate-set-field
             vim.ui.input = function(...)
-                require('lazy').load { plugins = { 'dressing.nvim' } }
+                if not package.loaded['dressing.nvim'] then
+                    require('lazy').load { plugins = { 'dressing.nvim' } }
+                end
                 return vim.ui.input(...)
             end
         end,
