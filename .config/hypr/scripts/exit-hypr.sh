b50e4222555e1a13346585a21dc7e2e96dc398b3
Maria José Solano <majosolano99@gmail.com>
2024-11-29 19:37:15 -0800

    fix(hypr): reformat script

--- .config/hypr/scripts/exit-hypr.sh
+++ .config/hypr/scripts/exit-hypr.sh
@@ -3,12 +3,12 @@
 # Taken from https://github.com/hyprwm/Hyprland/issues/3558#issuecomment-1950015566.
 
 if pgrep -x Hyprland >/dev/null; then
-	# Use the regular dispatch command.
-	hyprctl dispatch exit
-	# Pause for 2 seconds.
-	sleep 2
-	# If still alive, be violent.
-	if pgrep -x Hyprland >/dev/null; then
-		killall -9 Hyprland
-	fi
+    # Use the regular dispatch command.
+    hyprctl dispatch exit
+    # Pause for 2 seconds.
+    sleep 2
+    # If still alive, be violent.
+    if pgrep -x Hyprland >/dev/null; then
+        killall -9 Hyprland
+    fi
 fi
