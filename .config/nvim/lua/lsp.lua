7b4d642b3d4009c0ffe8c6076c73c68b56dafc93
Maria José Solano <majosolano99@gmail.com>
2024-12-29 07:36:46 -0800

    fix(nvim): make signature help focusable

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -172,7 +172,6 @@ local signature_help = vim.lsp.buf.signature_help
 vim.lsp.buf.signature_help = function()
     return signature_help {
         border = 'rounded',
-        focusable = false,
         max_height = math.floor(vim.o.lines * 0.5),
         max_width = math.floor(vim.o.columns * 0.4),
     }
