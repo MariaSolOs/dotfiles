3e3469bcf16a820f0af3a6c373184f8c1297daf6
Maria José Solano <majosolano99@gmail.com>
2025-02-09 14:44:15 -0800

    feat(nvim): add LSP support to renaming with mini.files

--- .config/nvim/lua/plugins/minifiles.lua
+++ .config/nvim/lua/plugins/minifiles.lua
@@ -122,6 +122,41 @@ return {
                 end,
             })
 
+            -- HACK: Notify LSPs that a file got renamed.
+            -- Borrowed this from snacks.nvim.
+            vim.api.nvim_create_autocmd('User', {
+                desc = 'Notify LSPs that a file was renamed',
+                pattern = 'MiniFilesActionRename',
+                callback = function(args)
+                    local changes = {
+                        files = {
+                            {
+                                oldUri = vim.uri_from_fname(args.data.from),
+                                newUri = vim.uri_from_fname(args.data.to),
+                            },
+                        },
+                    }
+                    local will_rename_method, did_rename_method =
+                        vim.lsp.protocol.Methods.workspace_willRenameFiles,
+                        vim.lsp.protocol.Methods.workspace_didRenameFiles
+                    local clients = vim.lsp.get_clients()
+                    for _, client in ipairs(clients) do
+                        if client:supports_method(will_rename_method) then
+                            local res = client:request_sync(will_rename_method, changes, 1000, 0)
+                            if res and res.result then
+                                vim.lsp.util.apply_workspace_edit(res.result, client.offset_encoding)
+                            end
+                        end
+                    end
+
+                    for _, client in ipairs(clients) do
+                        if client:supports_method(did_rename_method) then
+                            client:notify(did_rename_method, changes)
+                        end
+                    end
+                end,
+            })
+
             vim.api.nvim_create_autocmd('User', {
                 desc = 'Add rounded corners to minifiles window',
                 pattern = 'MiniFilesWindowOpen',
