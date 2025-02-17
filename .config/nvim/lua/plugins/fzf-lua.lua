884822ffaeb45863f210e5e00c207e5a07411ee0
Maria José Solano <majosolano99@gmail.com>
2025-02-17 15:50:26 -0800

    feat(nvim): directly use fzf-lua's select

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -109,5 +109,36 @@ return {
                 },
             }
         end,
+        init = function()
+            ---@diagnostic disable-next-line: duplicate-set-field
+            vim.ui.select = function(items, opts, on_choice)
+                local ui_select = require 'fzf-lua.providers.ui_select'
+
+                -- Register the fzf-lua picker the first time we call select.
+                if not ui_select.is_registered() then
+                    ui_select.register(function(ui_opts)
+                        if ui_opts.kind == 'luasnip' then
+                            ui_opts.prompt = 'Snippet choice: '
+                            ui_opts.winopts = {
+                                relative = 'cursor',
+                                height = 0.35,
+                                width = 0.3,
+                            }
+                        elseif ui_opts.kind == 'lsp_message' then
+                            ui_opts.winopts = { height = 0.4, width = 0.4 }
+                        else
+                            ui_opts.winopts = { height = 0.6, width = 0.5 }
+                        end
+
+                        return ui_opts
+                    end)
+                end
+
+                -- Don't show the picker if there's nothing to pick.
+                if #items > 0 then
+                    return vim.ui.select(items, opts, on_choice)
+                end
+            end
+        end,
     },
 }
