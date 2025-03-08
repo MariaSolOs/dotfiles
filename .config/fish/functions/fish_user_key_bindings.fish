ddfb96c798d671179ae5878afcc63b6a7d2950b8
Maria José Solano <majosolano99@gmail.com>
2025-03-08 10:34:58 -0800

    fix(fish): make sure ctrl-n still works in insert mode

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -3,9 +3,8 @@ function fish_user_key_bindings
     fish_default_key_bindings -M insert
     fish_vi_key_bindings --no-erase insert
 
-    # Keep the same history navigation bindings.
-    bind \cp up-or-search
-    bind \cn down-or-search
+    # Make sure ctrl-n still works in insert mode.
+    bind -M insert ctrl-n down-or-search
 
     # Replace !! by the previous command.
     bind -M insert ! bind_bang
