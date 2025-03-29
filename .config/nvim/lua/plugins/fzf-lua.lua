051d756aa1285789f27fbc80de58247fca46cc3a
Maria José Solano <majosolano99@gmail.com>
2025-03-29 10:57:16 -0700

    fix(nvim): use live_grep fzf-lua picker

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -24,7 +24,7 @@ return {
             { '<leader>fd', '<cmd>FzfLua lsp_document_diagnostics<cr>', desc = 'Document diagnostics' },
             { '<leader>fD', '<cmd>FzfLua lsp_workspace_diagnostics<cr>', desc = 'Workspace diagnostics' },
             { '<leader>ff', '<cmd>FzfLua files<cr>', desc = 'Find files' },
-            { '<leader>fg', '<cmd>FzfLua live_grep_glob<cr>', desc = 'Grep' },
+            { '<leader>fg', '<cmd>FzfLua live_grep<cr>', desc = 'Grep' },
             { '<leader>fg', '<cmd>FzfLua grep_visual<cr>', desc = 'Grep', mode = 'x' },
             { '<leader>fh', '<cmd>FzfLua help_tags<cr>', desc = 'Help' },
             {
