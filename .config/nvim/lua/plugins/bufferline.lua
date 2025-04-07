12b3017367eb1f5d32a07bda379ff768ecf2869b
Maria José Solano <majosolano99@gmail.com>
2025-04-06 18:25:06 -0700

    feat(nvim): remove buffer keymaps that are now defaults

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -22,8 +22,6 @@ return {
         },
         keys = {
             -- Buffer navigation.
-            { '[b', '<cmd>BufferLineCyclePrev<cr>', desc = 'Previous buffer' },
-            { ']b', '<cmd>BufferLineCycleNext<cr>', desc = 'Next buffer' },
             { '<leader>bp', '<cmd>BufferLinePick<cr>', desc = 'Pick a buffer to open' },
             { '<leader>bc', '<cmd>BufferLinePickClose<cr>', desc = 'Select a buffer to close' },
             { '<leader>bl', '<cmd>BufferLineCloseLeft<cr>', desc = 'Close buffers to the left' },
