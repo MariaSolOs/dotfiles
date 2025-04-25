f796110eb065a97102810a71426d1cd7000fb97b
Maria José Solano <majosolano99@gmail.com>
2025-04-25 15:53:37 -0700

    feat(nvim): use VS Code's TypeScript copy when available

--- .config/nvim/lsp/vtsls.lua
+++ .config/nvim/lsp/vtsls.lua
@@ -9,6 +9,21 @@ local jsts_settings = {
     },
 }
 
+local function get_global_tsdk()
+    -- Use VS Code's bundled copy if available.
+    local vscode_tsdk_path = '/Applications/%s/Contents/Resources/app/extensions/node_modules/typescript/lib'
+    local vscode_tsdk = vscode_tsdk_path:format 'Visual Studio Code.app'
+    local vscode_insiders_tsdk = vscode_tsdk_path:format 'Visual Studio Code - Insiders.app'
+
+    if vim.fn.isdirectory(vscode_tsdk) == 1 then
+        return vscode_tsdk
+    elseif vim.fn.isdirectory(vscode_insiders_tsdk) == 1 then
+        return vscode_insiders_tsdk
+    else
+        return nil
+    end
+end
+
 ---@type vim.lsp.Config
 return {
     cmd = { 'vtsls', '--stdio' },
@@ -18,6 +33,9 @@ return {
         typescript = jsts_settings,
         javascript = jsts_settings,
         vtsls = {
+            typescript = {
+                globalTsdk = get_global_tsdk(),
+            },
             -- Automatically use workspace version of TypeScript lib on startup.
             autoUseWorkspaceTsdk = true,
             experimental = {
