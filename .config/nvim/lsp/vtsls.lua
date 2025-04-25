f83e78837ad82a9da52dcd7173186dcd64c8776a
Maria José Solano <majosolano99@gmail.com>
2025-04-25 16:00:12 -0700

    feat(nvim): improve `vtsls` root detection for monorepos

--- .config/nvim/lsp/vtsls.lua
+++ .config/nvim/lsp/vtsls.lua
@@ -28,7 +28,19 @@ end
 return {
     cmd = { 'vtsls', '--stdio' },
     filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
-    root_markers = { 'tsconfig.json', 'jsonconfig.json' },
+    root_dir = function(bufnr, cb)
+        local fname = vim.uri_to_fname(vim.uri_from_bufnr(bufnr))
+
+        local ts_root = vim.fs.find('tsconfig.json', { upward = true, path = fname })[1]
+        -- Use the git root to deal with monorepos where TypeScript is installed in the root node_modules folder.
+        local git_root = vim.fs.find('.git', { upward = true, path = fname })[1]
+
+        if git_root then
+            cb(vim.fn.fnamemodify(git_root, ':h'))
+        elseif ts_root then
+            cb(vim.fn.fnamemodify(ts_root, ':h'))
+        end
+    end,
     settings = {
         typescript = jsts_settings,
         javascript = jsts_settings,
