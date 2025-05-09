074a9c7d6ed3cef1c514376cf6c8f4ef52160052
Maria José Solano <majosolano99@gmail.com>
2025-05-09 13:45:19 -0700

    feat(nvim): trying Copilot again

---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,21 @@
+-- Copilot completion.
+return {
+    {
+        'zbirenbaum/copilot.lua',
+        event = 'InsertEnter',
+        opts = {
+            -- The panel is useless.
+            panel = { enabled = false },
+            suggestion = {
+                keymap = {
+                    accept = '<C-.>',
+                    accept_word = '<M-w>',
+                    accept_line = '<M-l>',
+                    next = '<M-]>',
+                    prev = '<M-[>',
+                    dismiss = '<C-/>',
+                },
+            },
+        },
+    },
+}
