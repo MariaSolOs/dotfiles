bf0bcd8761309262a7b809eb321ed159eae8d31a
Maria José Solano <majosolano99@gmail.com>
2025-05-17 15:18:25 -0700

    fix(nvim): avoid `gr` codecompanion keymap conflicts

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -8,6 +8,21 @@ return {
             'CodeCompanionToggle',
             'CodeCompanionActions',
         },
-        opts = {},
+        opts = {
+            strategies = {
+                inline = {
+                    keymaps = {
+                        accept_change = {
+                            modes = { n = '<leader>ca' },
+                            description = 'Accept the suggested change',
+                        },
+                        reject_change = {
+                            modes = { n = '<leader>cr' },
+                            description = 'Reject the suggested change',
+                        },
+                    },
+                },
+            },
+        },
     },
 }
