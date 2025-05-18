30fdbe5d902de1c0145f02664888231f7af7f227
Maria José Solano <majosolano99@gmail.com>
2025-05-18 14:18:07 -0700

    fix(nvim): disable folding in `CodeCompanion` diffs

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -8,21 +8,31 @@ return {
             'CodeCompanionToggle',
             'CodeCompanionActions',
         },
-        opts = {
-            strategies = {
-                inline = {
-                    keymaps = {
-                        accept_change = {
-                            modes = { n = '<leader>ca' },
-                            description = 'Accept the suggested change',
-                        },
-                        reject_change = {
-                            modes = { n = '<leader>cr' },
-                            description = 'Reject the suggested change',
+        opts = function()
+            local config = require('codecompanion.config').config
+
+            local diff_opts = config.display.diff.opts
+            table.insert(diff_opts, 'context:99') -- Setting the context to a very large number disables folding.
+
+            return {
+                strategies = {
+                    inline = {
+                        keymaps = {
+                            accept_change = {
+                                modes = { n = '<leader>ca' },
+                                description = 'Accept the suggested change',
+                            },
+                            reject_change = {
+                                modes = { n = '<leader>cr' },
+                                description = 'Reject the suggested change',
+                            },
                         },
                     },
                 },
-            },
-        },
+                display = {
+                    diff = { opts = diff_opts },
+                },
+            }
+        end,
     },
 }
