c1808afdffebbd48d7fe39053e9108b90d5566e4
Maria José Solano <majosolano99@gmail.com>
2025-05-30 18:42:20 -0700

    feat(nvim): re-install `vim-sleuth`

--- .config/nvim/lua/plugins/ibl.lua
+++ .config/nvim/lua/plugins/ibl.lua
@@ -4,6 +4,8 @@ return {
         'lukas-reineke/indent-blankline.nvim',
         main = 'ibl',
         event = 'VeryLazy',
+        -- For setting shiftwidth and tabstop automatically.
+        dependencies = 'tpope/vim-sleuth',
         opts = {
             indent = {
                 char = require('icons').misc.vertical_bar,
