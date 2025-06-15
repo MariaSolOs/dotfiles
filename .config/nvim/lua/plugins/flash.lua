8cd2968ccdc50827304de2be144ea96b4b1f06da
Maria José Solano <majosolano99@gmail.com>
2025-06-14 18:43:04 -0700

    fix(nvim): remove stale `bqf` reference

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -17,11 +17,6 @@ return {
                     'flash_prompt',
                     'qf',
                     function(win)
-                        -- Floating windows from bqf.
-                        if vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(win)):match 'BqfPreview' then
-                            return true
-                        end
-
                         -- Non-focusable windows.
                         return not vim.api.nvim_win_get_config(win).focusable
                     end,
