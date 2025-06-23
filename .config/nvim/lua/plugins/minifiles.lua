a08dfc216ab101244a8a5db4cc645b387a67e77a
magoz <apps@magoz.is>
2025-06-23 19:52:20 +0200

    feature/notify-lsp-on-file-move

--- .config/nvim/lua/plugins/minifiles.lua
+++ .config/nvim/lua/plugins/minifiles.lua
@@ -122,11 +122,11 @@ return {
                 end,
             })
 
-            -- HACK: Notify LSPs that a file got renamed.
+            -- HACK: Notify LSPs that a file got renamed or moved.
             -- Borrowed this from snacks.nvim.
             vim.api.nvim_create_autocmd('User', {
                 desc = 'Notify LSPs that a file was renamed',
-                pattern = 'MiniFilesActionRename',
+                pattern = { "MiniFilesActionRename", "MiniFilesActionMove" },
                 callback = function(args)
                     local changes = {
                         files = {
