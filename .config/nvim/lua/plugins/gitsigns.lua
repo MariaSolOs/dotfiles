13df9648e45b03443ecae1965f31084c17f85ce8
Maria José Solano <majosolano99@gmail.com>
2025-07-05 13:52:53 -0700

    feat(nvim): `gh` integration in `gitsigns` config

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -24,6 +24,8 @@ return {
                 changedelete = { text = dashed_bar },
             },
             preview_config = { border = 'rounded' },
+            attach_to_untracked = true,
+            gh = true,
             on_attach = function(bufnr)
                 local gitlinker = require 'gitlinker'
                 local gs = package.loaded.gitsigns
