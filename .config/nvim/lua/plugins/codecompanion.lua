17359ed32766334ed91e4d2e6a061d44c184735e
Maria José Solano <majosolano99@gmail.com>
2025-07-26 14:40:03 -0700

    fix(nvim): keep `plenary` dependency

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -3,6 +3,7 @@ return {
     {
         'olimorris/codecompanion.nvim',
         cmd = 'CodeCompanion',
+        dependencies = { 'nvim-lua/plenary.nvim' },
         keys = {
             { '<leader>at', '<cmd>CodeCompanionChat Toggle<cr>', desc = 'Toggle CodeCompanion chat' },
             { '<leader>aa', '<cmd>CodeCompanionChat Add<cr>', desc = 'Add to CodeCompanion chat', mode = 'x' },
