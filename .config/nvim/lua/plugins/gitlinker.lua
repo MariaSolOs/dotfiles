fd50a2f3898d2edc4e992c5784cc3e421b066f9c
Maria José Solano <majosolano99@gmail.com>
2025-07-26 14:47:06 -0700

    feat(nvim): configure `gitlinker` with Palantir's URLs

--- .config/nvim/lua/plugins/gitlinker.lua
+++ .config/nvim/lua/plugins/gitlinker.lua
@@ -2,7 +2,18 @@
 return {
     {
         'linrongbin16/gitlinker.nvim',
-        opts = {},
+        opts = function()
+            return {
+                router = {
+                    browse = {
+                        ['^github%.palantir%.build'] = require('gitlinker.routers').github_browse,
+                    },
+                    blame = {
+                        ['^github%.palantir%.build'] = require('gitlinker.routers').github_blame,
+                    },
+                },
+            }
+        end,
         keys = {
             { '<leader>gc', '<cmd>GitLink<cr>', mode = { 'n', 'v' }, desc = 'Yank git link' },
             { '<leader>go', '<cmd>GitLink! blame<cr>', mode = { 'n', 'v' }, desc = 'Open git link' },
