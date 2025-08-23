1d2f68ca74e996e9f0e2ac80b3c31916b3da7fde
Maria José Solano <majosolano99@gmail.com>
2025-08-23 15:39:00 -0700

    feat(nvim): use vscode-like `fzf` files formatter

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -89,6 +89,7 @@ return {
                 defaults = { git_icons = false },
                 -- Configuration for specific commands.
                 files = {
+                    formatter = 'path.filename_first',
                     winopts = {
                         preview = { hidden = true },
                     },
@@ -142,6 +143,7 @@ return {
                     },
                 },
                 oldfiles = {
+                    formatter = 'path.filename_first',
                     include_current_session = true,
                     winopts = {
                         preview = { hidden = true },
