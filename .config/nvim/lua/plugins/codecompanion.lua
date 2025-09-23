67240e6bc2eef685d860877a9798a76096e155c2
Maria José Solano <majosolano99@gmail.com>
2025-09-22 19:53:33 -0700

    fix(nvim): remove `diff_opts` settings from CodeCompanion

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -8,41 +8,25 @@ return {
             { '<leader>at', '<cmd>CodeCompanionChat Toggle<cr>', desc = 'Toggle CodeCompanion chat' },
             { '<leader>aa', '<cmd>CodeCompanionChat Add<cr>', desc = 'Add to CodeCompanion chat', mode = 'x' },
         },
-        opts = function()
-            local config = require('codecompanion.config').config
-
-            local diff_opts = config.display.diff.opts
-            table.insert(diff_opts, 'context:99') -- Setting the context to a very large number disables folding.
-
-            return {
-                strategies = {
-                    inline = {
-                        keymaps = {
-                            accept_change = {
-                                modes = { n = '<leader>ay' },
-                                description = 'Accept the suggested change',
-                            },
-                            always_accept = {
-                                modes = { n = '<leader>aY' },
-                                description = 'Accept and enable auto mode',
-                            },
-                            reject_change = {
-                                modes = { n = '<leader>an' },
-                                description = 'Reject the suggested change',
-                            },
+        opts = {
+            strategies = {
+                inline = {
+                    keymaps = {
+                        accept_change = {
+                            modes = { n = '<leader>ay' },
+                            description = 'Accept the suggested change',
+                        },
+                        always_accept = {
+                            modes = { n = '<leader>aY' },
+                            description = 'Accept and enable auto mode',
+                        },
+                        reject_change = {
+                            modes = { n = '<leader>an' },
+                            description = 'Reject the suggested change',
                         },
                     },
                 },
-                display = {
-                    diff = {
-                        opts = diff_opts,
-                        diff_signs = {
-                            -- Disable signs for changes.
-                            signs = { text = '' },
-                        },
-                    },
-                },
-            }
-        end,
+            },
+        },
     },
 }
