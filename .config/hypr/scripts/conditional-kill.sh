26a631028e46cdcb6ad0a350d92e6c484fc88b73
Maria José Solano <majosolano99@gmail.com>
2025-10-19 17:51:33 -0700

    feat(hypr): add Signal to unkillable apps

--- .config/hypr/scripts/conditional-kill.sh
+++ .config/hypr/scripts/conditional-kill.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-# Close the window, except for Thunderbird and Obsidian.
-hyprctl activewindow -j | jq -r .title | grep -E "Mozilla Thunderbird|Obsidian" || hyprctl dispatch killactive
+# Close the window, except for some special apps.
+hyprctl activewindow -j | jq -r .title | grep -E "Mozilla Thunderbird|Obsidian|Signal" || hyprctl dispatch killactive
