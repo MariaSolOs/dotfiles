3112358f593c4c412ed43c61eb417ad09e4d8af6
Maria José Solano <majosolano99@gmail.com>
2025-11-11 10:39:35 -0800

    feat(nvim): nicer completion menu

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -22,7 +22,16 @@ return {
                     max_items = 10,
                 },
                 documentation = { auto_show = true },
-                menu = { scrollbar = false },
+                menu = {
+                    scrollbar = false,
+                    draw = {
+                        gap = 2,
+                        columns = {
+                            { 'kind_icon', 'kind', gap = 1 },
+                            { 'label', 'label_description', gap = 1 },
+                        },
+                    },
+                },
             },
             snippets = { preset = 'luasnip' },
             -- Disable command line completion:
