8370890cc814ecb1768e7f4a6c795a8ad7c45131
Maria Solano <majosolano99@gmail.com>
2026-01-31 22:54:03 -0800

    fix(nvim): add `root_dir` for `tsgo`

--- .config/nvim/lsp/tsgo.lua
+++ .config/nvim/lsp/tsgo.lua
@@ -4,4 +4,12 @@
 return {
     cmd = { 'tsgo', '--lsp', '--stdio' },
     filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+    root_dir = function(bufnr, on_dir)
+        local root_markers = { { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml' }, { '.git' } }
+
+        -- Fallback to the current working directory if no project root is found.
+        local project_root = vim.fs.root(bufnr, root_markers) or vim.fn.getcwd()
+
+        on_dir(project_root)
+    end,
 }
