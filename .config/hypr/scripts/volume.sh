9ddb01c53b930966ff78bea8251940c96d5cfa39
Maria Solano <majosolano99@gmail.com>
2026-02-01 10:33:48 -0800

    fix(hypr): `pamixer` maximum volume

--- .config/hypr/scripts/volume.sh
+++ .config/hypr/scripts/volume.sh
@@ -2,9 +2,9 @@
 
 # Change the volume according to the input flag.
 if [[ "$1" == "--dec" ]]; then
-    pamixer -d 5
+    pamixer -d 5 --set-limit 100
 elif [[ "$1" == "--inc" ]]; then
-    pamixer -i 5
+    pamixer -i 5 --set-limit 100
 elif [[ "$1" == "--toggle-mut" ]]; then
     pamixer -t
 fi
