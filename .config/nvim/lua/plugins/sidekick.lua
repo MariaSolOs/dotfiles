04d4742d8c3e9c3f1ea9caa1bfa6ef3eca731439
Maria Solano <majosolano99@gmail.com>
2026-02-13 14:13:38 -0800

    feat(nvim): let's use sidekick

---
+++ .config/nvim/lua/plugins/sidekick.lua
@@ -0,0 +1,28 @@
+-- Built-in terminal for Claude.
+return {
+    {
+        'folke/sidekick.nvim',
+        opts = {
+            -- Disable next-edit suggestions.
+            -- TODO: Give this another chance?
+            nes = { enabled = false },
+        },
+        keys = {
+            {
+                '<leader>at',
+                function()
+                    require('sidekick.cli').toggle { name = 'claude', focus = true }
+                end,
+                desc = 'Toggle Claude',
+            },
+            {
+                '<leader>av',
+                function()
+                    require('sidekick.cli').send { msg = '{selection}' }
+                end,
+                mode = { 'x' },
+                desc = 'Send visual selection to Sidekick',
+            },
+        },
+    },
+}
