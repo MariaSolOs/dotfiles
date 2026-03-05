7a05d04aa1a3193d0995228f22312e428f29fbd0
Maria Solano <majosolano99@gmail.com>
2026-03-05 11:55:42 -0800

    fix(nvim): add keymaps for deleting/surrounding pairs

--- .config/nvim/lua/plugins/nvim-surround.lua
+++ .config/nvim/lua/plugins/nvim-surround.lua
@@ -19,6 +19,12 @@ return {
             vim.keymap.set('x', 'Z', '<Plug>(nvim-surround-visual)', {
                 desc = 'Add a surrounding pair around a visual selection',
             })
+            vim.keymap.set('n', 'ds', '<Plug>(nvim-surround-delete)', {
+                desc = 'Delete a surrounding pair',
+            })
+            vim.keymap.set('n', 'cs', '<Plug>(nvim-surround-change)', {
+                desc = 'Change a surrounding pair',
+            })
         end,
     },
 }
