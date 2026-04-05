d06ed0cbc9a8af0094f04b0618c01fae12cfc95c
Maria Solano <majosolano99@gmail.com>
2026-04-05 13:47:40 -0700

    feat(nvim): `PluginSpec` type

--- .config/nvim/lua/vim-pack.lua
+++ .config/nvim/lua/vim-pack.lua
@@ -1,9 +1,15 @@
 local M = {}
 
+---@class PluginSpec
+---@field src string The GitHub repository of the plugin
+---@field module_name? string Optional module name for configuration (defaults to the repo name)
+---@field opts? table Optional configuration options for the plugin
+---@field on_setup? fun():nil Optional function to run after the plugin is loaded and configured
+
 --- Helper function for adding and configuring plugins to the current session on a specific event.
 ---
 ---@param event vim.api.keyset.events
----@param plugins { src: string, module_name?: string, opts?: table }[]
+---@param plugins PluginSpec[]
 function M.add_on_event(event, plugins)
     vim.api.nvim_create_autocmd(event, {
         once = true,
@@ -20,6 +26,10 @@ function M.add_on_event(event, plugins)
             for _, plugin in ipairs(plugins) do
                 local module_name = plugin.module_name or plugin.src:match '.+/(.+)'
                 require(module_name).setup(plugin.opts or {})
+
+                if plugin.on_setup then
+                    plugin.on_setup()
+                end
             end
         end,
     })
