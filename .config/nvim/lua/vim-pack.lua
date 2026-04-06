878bab34c1a8dc9f598c89fb9a7c58f017e8761e
Maria Solano <majosolano99@gmail.com>
2026-04-05 17:33:07 -0700

    feat(nvim): continue `vim.pack` migration

--- .config/nvim/lua/vim-pack.lua
+++ .config/nvim/lua/vim-pack.lua
@@ -35,4 +35,22 @@ function M.add_on_event(event, plugins)
     })
 end
 
+--- Runs the given command inside the plugin's directory when the plugin is updated.
+---
+---@param plugin_name string Plugin name
+---@param cmd string|fun():nil Command to run
+function M.on_plugin_update(plugin_name, cmd)
+    vim.api.nvim_create_autocmd('PackChanged', {
+        callback = function(args)
+            if args.data.spec.name == plugin_name and (args.data.kind == 'install' or args.data.kind == 'update') then
+                if type(cmd) == 'string' then
+                    vim.system({ cmd }, { cwd = args.data.path })
+                else
+                    cmd()
+                end
+            end
+        end,
+    })
+end
+
 return M
