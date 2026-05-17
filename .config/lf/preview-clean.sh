4a34ea5cc7c003dac7b94b6777d696ad2cfec9e4
Maria Solano <majosolano99@gmail.com>
2026-05-17 14:33:01 -0700

    fix(lf): use `stream` for image previews

--- .config/lf/preview-clean.sh
+++ .config/lf/preview-clean.sh
@@ -2,4 +2,4 @@
 
 # Cleaner script.
 # I don't really know what it does, I just took it from https://github.com/gokcehan/lf/wiki/Previews#with-kitty-and-pistol.
-kitty +kitten icat --clear --stdin no --silent --transfer-mode file </dev/null >/dev/tty
+kitty +kitten icat --clear --stdin no --silent --transfer-mode stream </dev/null >/dev/tty
--- .config/lf/preview.sh
+++ .config/lf/preview.sh
@@ -2,7 +2,7 @@
 
 # If the file is an image, use kitty to display it.
 if [[ "$(file -Lb --mime-type "$1")" =~ ^image ]]; then
-	kitty +kitten icat --silent --stdin no --transfer-mode file --place "${2}x${3}@${4}x${5}" "$1" </dev/null >/dev/tty
+	kitty +kitten icat --silent --stdin no --transfer-mode stream --place "${2}x${3}@${4}x${5}" "$1" </dev/null >/dev/tty
 	exit 1
 fi
 
