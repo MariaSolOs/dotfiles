ba4414c064b51f6d8bc9e430fb8b832f8ddf6fe4
Maria Solano <majosolano99@gmail.com>
2026-06-07 16:17:02 -0700

    fix(nvim): update `blink` before adding

--- .config/nvim/plugin/blink.lua
+++ .config/nvim/plugin/blink.lua
@@ -1,6 +1,12 @@
 local add = require('vim-pack').add
 local on_plugin_update = require('vim-pack').on_plugin_update
 
+on_plugin_update('blink.cmp', function()
+    -- TODO: Fix the Task type below.
+    ---@diagnostic disable-next-line: undefined-field
+    require('blink.cmp').build():pwait()
+end)
+
 -- Auto-completion and snippets.
 add {
     { src = 'saghen/blink.lib', setup = false },
@@ -111,9 +117,3 @@ add {
         },
     },
 }
-
-on_plugin_update('blink.cmp', function()
-    -- TODO: Fix the Task type below.
-    ---@diagnostic disable-next-line: undefined-field
-    require('blink.cmp').build():wait(60000)
-end)
